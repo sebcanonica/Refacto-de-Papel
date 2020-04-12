2008fc68b20f879fa05f94c3545a03f7e7ff832a
Sébastien Canonica <sebcano.biz@laposte.net>
2020-04-12 16:12:55 +0200

    Initial commit

---
+++ src/main/java/HeistCrew.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public class HeistCrew {
+    private int count;
+    final private int MAX_MEMBERS = 7;
+
+    // Just getter
+    private String[] codeNames = new String[MAX_MEMBERS];
+
+    // Several correlated members
+    private int[] ammoPerClip = new int[MAX_MEMBERS];
+    private int[] ammoLeftInLoadedClip = new int[MAX_MEMBERS];
+    private int[] clipsLeft = new int[MAX_MEMBERS];
+
+    // With side effect
+    private boolean[] wounded = new boolean[MAX_MEMBERS];
+    private int surgicalKitsLeft = 3;
+
+    public int count() {
+        return count;
+    }
+
+    public void enroll(String codeName, int clips, int ammoPerClip) {
+        codeNames[count] = codeName;
+        clipsLeft[count] = clips-1;
+        this.ammoPerClip[count] = ammoPerClip;
+        ammoLeftInLoadedClip[count] = ammoPerClip;
+        count++;
+    }
+
+    public int indexOf(String codeName) {
+        return Arrays.asList(codeNames).indexOf(codeName);
+    }
+
+    public String nameOf(int index) {
+        return codeNames[index];
+    }
+
+    public int fire(int index, int ammo) {
+        int leftToFire = ammo;
+        while (leftToFire > 0 && (ammoLeftInLoadedClip[index] > 0 || clipsLeft[index] > 0)) {
+            int firedFromLoadedClip = Math.min(leftToFire, ammoLeftInLoadedClip[index]);
+            leftToFire -= firedFromLoadedClip;
+            ammoLeftInLoadedClip[index] -= firedFromLoadedClip;
+            boolean shouldReload = ammoLeftInLoadedClip[index] == 0 && clipsLeft[index] > 0;
+            if (shouldReload) {
+                ammoLeftInLoadedClip[index] = ammoPerClip[index];
+                clipsLeft[index]--;
+            }
+        }
+        return ammo - leftToFire;
+    }
+
+    public int ammoLeft(int index) {
+        return clipsLeft[index]*ammoPerClip[index]+ammoLeftInLoadedClip[index];
+    }
+
+    public void setWounded(int index) {
+        wounded[index] = true;
+    }
+
+    public void heal(int index) {
+        if (surgicalKitsLeft > 0) {
+            surgicalKitsLeft--;
+            wounded[index] = false;
+        }
+    }
+
+    public boolean isHealthy(int index) {
+        return ! wounded[index];
+    }
+}
---
+++ src/test/java/HeistCrewTest.java
@@ -0,0 +1,79 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.stream.IntStream;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class HeistCrewTest {
+    private HeistCrew crew = new HeistCrew();
+
+    @Test
+    public void can_be_empty() {
+        assertThat(crew.count()).isEqualTo(0);
+    }
+
+    @Test
+    public void can_have_7_members() {
+        enrollFullSquad();
+        assertThat(crew.count()).isEqualTo(7);
+    }
+
+    @Test
+    public void query_by_code_name() {
+        enrollFullSquad();
+        assertThat(crew.indexOf("Ber" + "lin")).isEqualTo(4);
+        assertThat(crew.indexOf("Stockholm")).isEqualTo(-1);
+    }
+
+    @Test
+    public void get_code_name() {
+        enrollFullSquad();
+        assertThat(crew.nameOf(4)).isEqualTo("Berlin");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 6, 6, 54",
+            "1, 15, 15, 135",
+            "2, 12, 12, 48",
+            "3, 20, 20, 85",
+            "4, 10, 5, 0",
+            "6, 10, 0, 0"
+    })
+    public void order_fire(int index, int ammo, int expectedFired, int expectedLeft) {
+        enrollFullSquad();
+        int bulletFired = crew.fire(index, ammo);
+        assertThat(bulletFired).isEqualTo(expectedFired);
+        assertThat(crew.ammoLeft(index)).isEqualTo(expectedLeft);
+    }
+
+    @Test
+    public void heal_wounded() {
+        enrollFullSquad();
+        crew.setWounded(0);
+        crew.heal(0);
+        assertThat(crew.isHealthy(0)).isTrue();
+    }
+
+    @Test
+    public void cannot_heal_more_than_3_times() {
+        enrollFullSquad();
+        IntStream.range(0, 4).forEach(i -> {
+            crew.setWounded(i);
+            crew.heal(i);
+        });
+        assertThat(crew.isHealthy(3)).isFalse();
+    }
+
+    private void enrollFullSquad() {
+        crew.enroll("Tokyo", 10, 6);
+        crew.enroll("Nairobi", 5, 30);
+        crew.enroll("Denver", 10, 6);
+        crew.enroll("Rio", 7, 15);
+        crew.enroll("Berlin", 5, 1);
+        crew.enroll("Helsinki", 2, 100);
+        crew.enroll("Moscow", 0, 0);
+    }
+}
